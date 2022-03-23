c68957a36953117e2536d1a9e1cb2763b51c512f
Bichun Zhang <bza62@sfu.ca>
2022-03-22 19:17:45 -0700

    Add heatmap

--- UNet_framework/diffmap.py
+++ UNet_framework/diffmap.py
@@ -4,6 +4,7 @@ from os import listdir
 from os.path import splitext
 import cv2
 import numpy as np
+import plotly.express as px
 
 def RGBtoGray(image):
     r, g, b = image[:, :, 0], image[:, :, 1], image[:, :, 2]
@@ -41,9 +42,9 @@ def GetDiffMap(preds, labels, inputs):
     dir2 = os.path.join(data_dir, "diff_input_GT")
 
     for i in range(n):
-        input = inputs[i]
-        label = labels[i]
-        pred = preds[i]
+        input = inputs[i].permute(1,2,0)
+        label = labels[i].permute(1,2,0)
+        pred = preds[i].permute(1,2,0)
         diff_input_pred = compare(input, pred)
         diff_input_GT = compare(input, label)
 
@@ -52,3 +53,32 @@ def GetDiffMap(preds, labels, inputs):
 
         cv2.imwrite(ip_dir, diff_input_pred)
         cv2.imwrite(ig_dir, diff_input_GT)
+
+def get_heat_map(preds, labels, inputs,dir_name):
+    n = len(preds)
+    root_dir = os.getcwd()
+
+    heatmap_dir = os.path.join(root_dir, dir_name)
+    heatmap_inputs_label_dir = os.path.join(heatmap_dir, 'input_label')
+    heatmap_pred_label_dir = os.path.join(heatmap_dir, 'pred_label')
+    if not os.path.exists(heatmap_inputs_label_dir):
+        os.mkdirs(heatmap_inputs_label_dir)
+    if not os.path.exists(heatmap_pred_label_dir):
+        os.mkdirs(heatmap_pred_label_dir)
+    for i in range(n):
+        input = inputs[i]
+        label = labels[i]
+        pred = preds[i]
+        input = RGBtoGray(input)
+        label = RGBtoGray(label)
+        pred = RGBtoGray(pred)
+        heat_map_pred_label = pred - label
+        heat_map_input_label = input - label
+        fig_pred_label = px.imshow(heat_map_pred_label)
+        fig_input_label = px.imshow(heat_map_input_label)
+
+        fig_pred_label.show()
+        fig_input_label.show()
+
+        fig_pred_label.write_image(heatmap_pred_label_dir+'/'+str(i)+'_pred_label.png')
+        fig_input_label.write_image(heatmap_inputs_label_dir+'/'+str(i)+'_input_label.png')
